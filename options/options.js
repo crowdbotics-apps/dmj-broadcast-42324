6c8c5ab3d9662abf0d2ceed75b867d09b1528e2f
Crowdbotics <team@crowdbotics.com>
2023-07-21 01:03:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dmj-broadcast-42324",
+  url: "https://dmj-broadcast-42324.botics.co",
+  api: "https://dmj-broadcast-42324.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
